3ac67c97f094eae2457f8d55847bb25bf81ce434
Pysia <bkubinska111@gmail.com>
2024-01-26 12:45:08 +0100

    delete podstrona.html

--- licznik.js
+++
@@ -1,5 +0,0 @@
-function zliczKlikniecia() {
-    let licznik = localStorage.getItem('licznik') || 0;
-    licznik = parseInt(licznik, 10) + 1;
-    localStorage.setItem('licznik', licznik);
-}
--- licznik_podstrona.js
+++
@@ -1,9 +0,0 @@
-function pobierzLicznik() {
-    const savedLicznik = sessionStorage.getItem('licznik');
-    if (savedLicznik) {
-        document.getElementById('licznik').textContent = savedLicznik;
-    }
-}
-
-// Pobierz początkowy stan licznika
-pobierzLicznik();
